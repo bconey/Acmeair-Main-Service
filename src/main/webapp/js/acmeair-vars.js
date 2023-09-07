2169a676c232d7de14f41fcd40d42063046c31b8
Daniel-Guinan <root@dgrhel91.fyre.ibm.com>
2023-09-07 10:45:57 -0700

    js changes

---
+++ src/main/webapp/js/acmeair-vars.js
@@ -0,0 +1,19 @@
+/*******************************************************************************
+* Copyright (c) 2013-2015 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+var authHost = 'app-coney-0822-aaauth.staging.us-east-1.c1.appflow.dev.ibmappdomain.cloud';
+var bookingHost = 'app-coney-0822-aabook.staging.us-east-1.c1.appflow.dev.ibmappdomain.cloud';
+var customerHost = 'app-coney-0822-aacust.staging.us-east-1.c1.appflow.dev.ibmappdomain.cloud';
+var flightHost = 'app-coney-0822-aaflight.staging.us-east-1.c1.appflow.dev.ibmappdomain.cloud';
